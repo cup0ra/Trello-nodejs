4cdaea1d9ea158ae6b1b596f7ee0d75449799ec7
sergey <57291691+cup0ra@users.noreply.github.com>
2021-06-06 22:39:35 +0300

    feat: added error handling

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 import { loggers } from './middleware/logging';
+import { BaseError, handleError, uncaughtOrUnhandledError } from './common/errorHandler';
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -26,5 +27,14 @@ app.use(loggers);
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards/:boardId/tasks', taskRouter);
+
+app.use(handleError);
+
+process.on('unhandledRejection', (err: BaseError) => uncaughtOrUnhandledError(err, 'Rejection:'));
+
+process.on('uncaughtException', (err: BaseError) => uncaughtOrUnhandledError(err, 'Exception:'));
+
+/* Promise.reject(Error('Oops!')); */
+/* throw Error('Oops!'); */
 
 export default app;
---
+++ src/common/statusCodes.ts
@@ -0,0 +1,7 @@
+import {
+  StatusCodes,
+} from 'http-status-codes';
+
+export const {
+  OK, BAD_REQUEST, CREATED, NOT_FOUND,
+} = StatusCodes;
